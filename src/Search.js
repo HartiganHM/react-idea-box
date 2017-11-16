91992261637dff3bdc45048b89429cda55af9716
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-11-16 14:26:31 -0700

    Built out all files for Search component

---
+++ src/Search.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './styles/Search.scss';
+
+export default class Search extends Component {
+    render() {
+        return (
+            <div>
+                <input type="text" value="" placeholder="Search"/>
+            </div>
+        )
+    }
+}
