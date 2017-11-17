84ea98266a3a2178f2601ae565e6c4502973f659
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-11-17 08:52:09 -0700

    Built out input handlers for Header

--- src/Header.js
+++ src/Header.js
@@ -8,14 +8,31 @@ export default class Header extends Component {
             title: '',
             body: ''
         }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
+
+    handleInputChange(event) {
+        let value = event.target.value;
+
+        if (event.target.type === 'textarea') {
+            this.setState({
+                body: value
+            })
+        } else {
+            this.setState({
+                title: value
+            })
+        }
+    }
+
     render() {
         return (
             <div className="Header">
                 <h1 className="page-header"><span className="accent-color">ireact</span>box</h1>
                 <form className="submit-form">
-                    <input className="submit-input" type="text" value="" placeholder="Title"/>
-                    <textarea className="submit-input" type="textarea" value="" placeholder="Body"/>
+                    <input className="submit-input" type="text" value={this.state.title} placeholder="Title" onChange={this.handleInputChange}/>
+                    <textarea className="submit-input" type="textarea" value={this.state.body} placeholder="Body" onChange={this.handleInputChange}/>
                     <button className="submit-input submit-button" >Save</button>
                 </form>
             </div>
