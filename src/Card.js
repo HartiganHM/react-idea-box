ad3ec314be34f0a0d79acb35bade8518394e42e5
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-11-20 10:43:03 -0700

    Destructured card props

--- src/Card.js
+++ src/Card.js
@@ -3,19 +3,20 @@ import './styles/Card.css';
 
 export default class Card extends Component {
     render () {
+        let { title, body, quality, id } = this.props;
         return (
-            <div className="Card" id={this.props.id}>
+            <div className="Card" id={id}>
                 <div className="card-top">
-                    <span className="card-title">{this.props.title}</span>
+                    <span className="card-title">{title}</span>
                     <div className="card-button delete-button"></div>
                 </div>
 
-                <span className="card-body">{this.props.body}</span>
+                <span className="card-body">{body}</span>
 
                 <div className="card-bottom">
                     <div className="card-button up-button"></div>
                     <div className="card-button down-button"></div>
-                    <span className="quality">quality: <span className="card-quality">{this.props.quality}</span></span>
+                    <span className="quality">quality: <span className="card-quality">{quality}</span></span>
                 </div>
             </div>
         )
