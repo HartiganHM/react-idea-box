256380d82161d13bf91316901103b860711fdb05
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-11-20 10:17:26 -0700

    Passed props from Container to Card for conditional rendering

--- src/Card.js
+++ src/Card.js
@@ -4,18 +4,18 @@ import './styles/Card.css';
 export default class Card extends Component {
     render () {
         return (
-            <div className="Card">
+            <div className="Card" id={this.props.id}>
                 <div className="card-top">
-                    <span className="card-title">Example Idea 1</span>
+                    <span className="card-title">{this.props.title}</span>
                     <div className="card-button delete-button"></div>
                 </div>
 
-                <span className="card-body">Tiramisu carrot cake fruitcake gingerbread bear claw powder icing tootsie rool ice cream.</span>
+                <span className="card-body">{this.props.body}</span>
 
                 <div className="card-bottom">
                     <div className="card-button up-button"></div>
                     <div className="card-button down-button"></div>
-                    <span className="quality">quality: <span className="card-quality">swill</span></span>
+                    <span className="quality">quality: <span className="card-quality">{this.props.quality}</span></span>
                 </div>
             </div>
         )
