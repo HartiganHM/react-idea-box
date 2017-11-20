d5fde75b1e0ac8af75e5d7999f23d9a9e8308600
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-11-20 10:18:09 -0700

    Mapped cards from Container component and reversed array to prepend

--- src/Container.js
+++ src/Container.js
@@ -4,10 +4,22 @@ import './styles/Container.css';
 
 export default class Container extends Component {
     render () {
+        let reverseCards = this.props.cards.reverse();
         return (
             <div className="Container">
                 <input className="submit-input" type="text" value="" placeholder="Search"/>
-                <Card />
+                {
+                    reverseCards.map( (card, index) => {
+                        return (
+                            <Card
+                                title={card.title}
+                                body={card.body}
+                                quality={card.quality}
+                                id={card.id}
+                                key={index}/>
+                        )
+                    })
+                }
             </div>
         )
     }
