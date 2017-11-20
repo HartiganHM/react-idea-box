e17d8218dee15509df4fd186a2a5aac525c814e1
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-11-20 10:54:42 -0700

    Built handleClick method and built functionality for delete card from DOM and LS

--- src/Card.js
+++ src/Card.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import './styles/Card.css';
 
 export default class Card extends Component {
+    constructor() {
+        super();
+        this.state = {};
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event) {
+        if(event.target.className.includes('delete')) {
+            document.getElementById(this.props.id).remove();
+            localStorage.removeItem(JSON.stringify(this.props.id));
+        }
+    }
+
     render () {
         let { title, body, quality, id } = this.props;
         return (
-            <div className="Card" id={id}>
+            <div
+                className="Card"
+                id={id}
+                onClick={this.handleClick}>
                 <div className="card-top">
                     <span className="card-title">{title}</span>
                     <div className="card-button delete-button"></div>
