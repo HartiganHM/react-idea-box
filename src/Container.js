193212a99be6d9eb0cd8558b36aab2df1e2685da
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-11-22 16:41:22 -0700

    Parsed cards from App (ie LS) and pased to Card component

--- src/Container.js
+++ src/Container.js
@@ -4,19 +4,22 @@ import './styles/Container.css';
 
 export default class Container extends Component {
     render () {
-        let reverseCards = this.props.cards.reverse();
+        let keys = Object.keys(this.props.cards);
         return (
             <div className="Container">
                 <input className="submit-input" type="text" value="" placeholder="Search"/>
                 {
-                    reverseCards.map( (card, index) => {
+                    keys.reverse().map( (card, index) => {
+                        let parsedCard = JSON.parse(this.props.cards[card])
                         return (
                             <Card
-                                title={card.title}
-                                body={card.body}
-                                quality={card.quality}
-                                id={card.id}
-                                key={index}/>
+                                title={parsedCard.title}
+                                body={parsedCard.body}
+                                quality={parsedCard.quality}
+                                id={parsedCard.id}
+                                key={index}
+                                updateCardQuality={this.props.updateCardQuality}
+                                removeCard={this.props.removeCard}/>
                         )
                     })
                 }
