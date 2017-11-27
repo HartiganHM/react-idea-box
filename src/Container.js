a53f38097046ee60622530260047bb7389f5eb1d
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-11-26 20:38:07 -0700

    Built constructor and handleSearchChange for Container component; fixed spacing

--- src/Container.js
+++ src/Container.js
@@ -3,11 +3,35 @@ import Card from './Card';
 import './styles/Container.css';
 
 export default class Container extends Component {
+    constructor() {
+        super();
+        this.state = {
+            searchValue: ''
+        }
+
+        this.handleSearchChange = this.handleSearchChange.bind(this);
+    }
+
+    handleSearchChange(event) {
+        let value = event.target.value;
+
+        this.setState({
+            searchValue: value
+        })
+
+        this.props.searchCards(value);
+    }
+
     render () {
         let keys = Object.keys(this.props.cards);
         return (
             <div className="Container">
-                <input className="submit-input" type="text" value="" placeholder="Search"/>
+                <input
+                    className="submit-input"
+                    type="text"
+                    value={this.state.searchValue}
+                    placeholder="Search"
+                    onChange={this.handleSearchChange}/>
                 {
                     keys.reverse().map( (card, index) => {
                         let parsedCard = JSON.parse(this.props.cards[card])
